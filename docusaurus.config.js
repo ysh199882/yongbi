33fb633c9cc0eac7ca17b6e22c2644e35c9abe5d
ysh199882 <1781809824@qq.com>
2022-08-06 14:11:42 +0800

    style:样式修改

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '拥碧',
+  title: 'Yongbi',
   tagline: 'Just for today',
   url: 'https://yongbi.co',
   baseUrl: '/',
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,10 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import logo from '@site/static/img/头像.jpg'
-
+import beauty from '@site/static/img/beauty.jpeg'
 
 import styles from './index.module.css';
+
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -15,19 +16,20 @@ function HomepageHeader() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
       <img src={logo} style={{width: 280, borderRadius: '50%'}} /> 
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        {/* <div className={styles.buttons}>
+        <h1 className={styles.heroBannerTitle}>{siteConfig.title}</h1>
+        {/* <p className="hero__subtitle">{siteConfig.tagline}</p> */}
+        <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
 技术文章 →
           </Link>
-        </div> */}
+        </div>
       </div>
     </header>
   );
 }
+
 
 export default function Home() {
   const {siteConfig} = useDocusaurusContext();
