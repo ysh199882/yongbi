46be296fa9fe1e22849d8094a712748bbe762cb4
ysh199882 <1781809824@qq.com>
2022-08-09 18:22:51 +0800

    fix:路由不存在部署失败问题

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,15 +22,15 @@ const config = {
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'zh-Hans',
-    locales: ['zh-Hans','en'],
-    localeConfigs: {
-      en: {
-        htmlLang: 'en-GB',
-      },
-    },
-  },
+  // i18n: {
+  //   defaultLocale: 'zh-Hans',
+  //   locales: ['zh-Hans','en'],
+  //   localeConfigs: {
+  //     en: {
+  //       htmlLang: 'en-GB',
+  //     },
+  //   },
+  // },
 
   presets: [
     [
